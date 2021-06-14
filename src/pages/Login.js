463c89c9b657fb11f2cf2b276c4576870e568d17
Danny Gavin <homevpn14@gmail.com>
2021-06-14 09:56:02 -0700

    Fixed the login page to show a throbber before the user is logged in

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,8 @@
+import { registerPlugin } from 'axe-core'
 import { useContext, useState } from 'react'
 import {Link} from 'react-router-dom'
 import { GlobalCtx } from '../App'
+import loadGif from '../components/Loading.gif'
 
 const Login = (props) => {
 
@@ -14,6 +16,8 @@ const Login = (props) => {
 
     const [form, setForm] = useState(blank)
     const [errorText, setErrorText] = useState("")
+    const [submit, setSubmit] = useState(<input type="submit" value="Login" id="login"/>)
+    const [register, setRegister] = useState(<><p>Dont have an account?</p><Link to="/signup"><p id="margin-space">Register</p></Link></>)
 
     const handleChange = (event) => {
         setForm({...form, [event.target.name]: event.target.value})
@@ -22,7 +26,8 @@ const Login = (props) => {
     const handleSubmit = (event) => {
         event.preventDefault()
         const {username, password} = form
-
+        setSubmit(<img id="load-gif" src={loadGif} alt="loading" />)
+        setRegister(null)
         fetch(`${url}/auth/login`, {
             method: "POST",
             headers: {
@@ -35,8 +40,12 @@ const Login = (props) => {
             if (data.error === "User Does Not Exist") {
                 console.log("user failed")
                 setErrorText("Username does not exist.")
+                setSubmit(<input type="submit" value="Login" id="login"/>)
+                setRegister(<><p>Dont have an account?</p><Link to="/signup"><p id="margin-space">Register</p></Link></>)
             } else if (data.error === "PASSWORD DOES NOT MATCH") {
                 setErrorText("Password does not match.")
+                setSubmit(<input type="submit" value="Login" id="login"/>)
+                setRegister(<><p>Dont have an account?</p><Link to="/signup"><p id="margin-space">Register</p></Link></>)
             } else {
             localStorage.setItem("token", JSON.stringify(data))
             setGState({...gState, token: data.token})
@@ -60,12 +69,11 @@ const Login = (props) => {
             <form onSubmit={handleSubmit}>
                 <input type="text" name="username" value={form.username} onChange={handleChange} placeholder="Username" onKeyDown={preventSpace} minLength="3" maxLength="15" required/>
                 <input type="password" name="password" value={form.password} onChange={handleChange} placeholder="Password" minLength="3" maxLength="30" required/>
-                <input type="submit" value="Login" id="login"/>
+                {submit}
             </form>
         </section>
         <div id="form-bottom">
-        <p>Dont have an account?</p>
-        <Link to="/signup"><p id="margin-space">Register</p></Link>
+        {register}
         </div>
         </>  
     )
