3a6f57fb4f59a446384c68cad0163ffbda1fb089
Danny Gavin <homevpn14@gmail.com>
2021-06-16 17:53:02 -0700

    Had to get rid of pictures :( for now.. Heroku is not liking the large amounts of data.

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -189,7 +189,7 @@ const MyProfile = () => {
                                 <h2>{post.username}</h2>
                                 <h3>{moment(post.createdAt).format('MM-DD-YYYY')}</h3>
                             </section>
-                            <img src={post.image} alt="Post created by you"/>
+                            {/* <img src={post.image} alt="Post created by you"/> */}
                             <h3 id="post-note">{currentID === post._id ? editForm : post.note}</h3>
                             <hr/>
                             <section id="post-footer">
